bea23feb6a208994aef52ef05eb609a56cb34eec
Quan Nguyen <QuanNCT@outlook.com>
2023-04-20 07:47:18 +0700

    Fix code based on feedback of reviewers

--- opt/airflow/dags/common/operators/data_quality.py
+++ opt/airflow/dags/common/operators/data_quality.py
@@ -9,7 +9,7 @@ class DataQualityOperator(BaseOperator):
                  # Define your operators params (with defaults) here
                  # Example:
                  connection_id=None,
-                 tables=None,
+                 dq_checks=None,
                  *args, **kwargs):
         super(DataQualityOperator, self).__init__(*args, **kwargs)
 
@@ -19,23 +19,32 @@ class DataQualityOperator(BaseOperator):
                 "redshift": ""
             }
 
-        if tables is None:
-            tables = []
-
+        if dq_checks is None:
+            dq_checks = []
         # Map params here
         # Example:
         self.connection_id = connection_id
-        self.tables = tables
+        self.dq_checks = dq_checks
 
     def execute(self, context):
         postgres_hook = PostgresHook(self.connection_id["redshift"])
 
-        for table in self.tables:
-            records = postgres_hook.get_records("SELECT COUNT(*) FROM {table}".format(table=table))
+        if len(self.dq_checks) > 0:
+            self.log.info("Beginning to check data quality")
+            for i, dq_check in enumerate(self.dq_checks):
+                if dq_check["test_sql"] is not None and len(dq_check["test_sql"]) > 0:
+                    self.log.info("Checking {} ".format(i))
+                    records = postgres_hook.get_records(dq_check['test_sql'])
 
-            if len(records) < 1 or len(records[0]) < 1 or records[0][0] < 1:
-                self.log.info("Data quality check failed. {table} returned no results".format(table=table))
-
-            self.log.info("Data quality on table {table} check passed with {records} records".format(table=table,
-                                                                                                     records=records[0][
-                                                                                                         0]))
+                    if dq_check['expected_results'] != records[0][0]:
+                        raise ValueError("Data quality check #{} failed. Expected: {}, actual: {}".format(
+                            i,
+                            dq_check['expected_results'],
+                            records[0][0]))
+                    else:
+                        self.log.info("Data quality check #{} success. Expected: {}, actual: {}".format(
+                            i,
+                            dq_check['expected_results'],
+                            records[0][0]))
+        else:
+            self.log.info("Skipped to check data quality")
